087cd3fa8487e524106a87609b52fb46b15a519d
Brandon <sandovalbrandon20@gmail.com>
2025-05-29 19:30:13 -0700

    from last commit added to ignore .env and added connection to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Middleware to parse JSON bodies
 app.use(express.json());
