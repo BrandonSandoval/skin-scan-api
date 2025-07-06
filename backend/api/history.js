5f884fd5ea09f19f5291668d94ab054c67981e31
Brandon <sandovalbrandon20@gmail.com>
2025-07-06 00:25:44 -0700

    Added more flexibility with history

--- backend/api/history.js
+++ backend/api/history.js
@@ -5,8 +5,5 @@ const { getHistory } = require('../controllers/historyController');
 
 // TODO: Implement route logic
 router.get('/', authMiddleware, getHistory);
-router.get('/protected', authMiddleware, (req, res) => {
-    res.json({ message: `Hello user ${req.user.userId}, access granted!` });
-});
 
 module.exports = router;
